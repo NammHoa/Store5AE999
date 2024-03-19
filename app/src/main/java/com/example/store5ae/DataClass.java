45a8a9d5f3a3b28d9386dde315da3cb3b8d4bd91
NammHoa <119406080+NammHoa@users.noreply.github.com>
2024-03-19 11:48:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/store5ae/DataClass.java
@@ -0,0 +1,52 @@
+package com.example.store5ae;
+
+public class DataClass {
+    private String dataTitle;
+    private String dataDesc;
+    private String dataLang;
+    private String dataImage;
+    private String key;
+
+    public DataClass(String dataTitle, String dataDesc, String dataLang, String dataImage) {
+        this.dataTitle = dataTitle;
+        this.dataDesc = dataDesc;
+        this.dataLang = dataLang;
+        this.dataImage = dataImage;
+
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public DataClass(){
+
+}
+
+
+
+
+
+
+
+
+    public String getDataTitle() {
+        return dataTitle;
+    }
+
+    public String getDataDesc() {
+        return dataDesc;
+    }
+
+    public String getDataLang() {
+        return dataLang;
+    }
+
+    public String getDataImage() {
+        return dataImage;
+    }
+}
